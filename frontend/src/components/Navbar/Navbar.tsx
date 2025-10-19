8020920147af72e7d8be3f2934176ab66fbc1381
RichQ-hub <74640863+RichQ-hub@users.noreply.github.com>
2025-10-19 23:25:11 +1100

    Created Image Carousel and Navbar components

---
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className='fixed top-0 right-0 left-0 h-14 bg-gray-900 flex items-center z-50 px-4'>
+        Navigation bar
+    </nav>
+  )
+}
+
+export default Navbar;
---
+++ frontend/src/fronts.ts
@@ -0,0 +1,11 @@
+import { Fira_Sans, Nova_Square, Saira_Semi_Condensed } from 'next/font/google';
+ 
+export const fira = Fira_Sans({
+  weight: ['400', '500', '700'],
+  subsets: ['latin'],
+});
+
+export const saira = Saira_Semi_Condensed({
+  weight: ['400', '500', '600', '700'],
+  subsets: ['latin'],
+});
