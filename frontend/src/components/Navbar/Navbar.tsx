713897fe0a6e2349520d2a4366edfecac40b55f3
RichQ-hub <74640863+RichQ-hub@users.noreply.github.com>
2025-10-20 00:43:29 +1100

    Completed navbar component

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,9 +1,22 @@
+import Image from 'next/image';
 import React from 'react';
+import Navlinks from './Navlinks';
 
 const Navbar = () => {
   return (
-    <nav className='fixed top-0 right-0 left-0 h-14 bg-gray-900 flex items-center z-50 px-4'>
-        Navigation bar
+    <nav className='fixed top-0 right-0 left-0 h-14 bg-gray-900 flex items-center z-50 px-4 overflow-hidden'>
+      {/* Nib Logo */}
+      <div className='flex items-center justify-center'>
+        <Image
+          src='/nib_logo.gif'
+          alt='Nib Logo'
+          width={60}
+          height={60}
+          unoptimized
+        />
+      </div>
+      
+      <Navlinks />
     </nav>
   )
 }
---
+++ frontend/src/components/Navbar/Navlinks.tsx
@@ -0,0 +1,35 @@
+import { saira } from '@/fronts';
+import Link from 'next/link';
+import React from 'react';
+
+const NAV_LINKS = [
+  {
+    name: 'HOME',
+    href: '/',
+  },
+  {
+    name: 'CATS',
+    href: '/cats',
+  },
+]
+
+const Navlinks = () => {
+  return (
+    <ul className={`${saira.className} font-bold text-lg h-full flex items-center ml-4 text-zinc-400`}>
+      {NAV_LINKS.map((link, idx) => {
+        return (
+          <li key={idx} className='h-full'>
+            <Link
+              className='h-full px-3 hover:text-white flex items-center'
+              href={link.href}
+            >
+              {link.name}
+            </Link>
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
+
+export default Navlinks;
